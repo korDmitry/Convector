6b64f7ff17486b2fdb11d90b3f0890dbd62d9f8c
Korobov <korobov.nnov@gmail.com>
2017-08-16 00:09:46 -0400

    Some UI fixes

--- Convector/ConvectorViewController.swift
+++ Convector/ConvectorViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ConvectorViewController: UIViewController {
     
+    override func viewDidLoad() {
+        valueLabel.layer.addBorder(edge: .bottom, color: UIColor.white, thickness: 0.5)
+    }
+    
     //MARK: @IBOutlets
     
     @IBOutlet weak var firstMeasurePickerView: UIPickerView!
@@ -159,5 +163,30 @@ extension ConvectorViewController: UIPickerViewDataSource, UIPickerViewDelegate 
         }
         resultLabel.text = String(brain.calculateResult(Double(valueLabelText)!))
         updateUI()
+    }
+}
+
+
+extension CALayer {
+    func addBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
+        let border = CALayer()
+        switch edge {
+        case UIRectEdge.top:
+            border.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: thickness)
+            break
+        case UIRectEdge.bottom:
+            border.frame = CGRect(x: 0, y: self.frame.height - thickness, width: self.frame.width, height: thickness)
+            break
+        case UIRectEdge.left:
+            border.frame = CGRect(x: 0, y: 0, width: thickness, height: self.frame.height)
+            break
+        case UIRectEdge.right:
+            border.frame = CGRect(x: self.frame.width - thickness, y: 0, width: thickness, height: self.frame.height)
+            break
+        default:
+            break
+        }
+        border.backgroundColor = color.cgColor;
+        self.addSublayer(border)
     }
 }
