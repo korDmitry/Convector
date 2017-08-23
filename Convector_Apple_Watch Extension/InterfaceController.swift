f28b6541bdc15ee6859750fa5b41ca5928eb740d
Korobov <korobov.nnov@gmail.com>
2017-08-22 21:10:13 -0400

    Add WatchApp and WatchKitExtension

---
+++ Convector_Apple_Watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Convector_Apple_Watch Extension
+//
+//  Created by Дмитрий Коробов on 22.08.17.
+//  Copyright © 2017 Дмитрий Коробов. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
